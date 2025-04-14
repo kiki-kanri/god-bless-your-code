7e72f77825d563bb4fa367bec5d511fd75567e9a
kiki-kanri <a470666@gmail.com>
2025-04-14 21:56:31 +0800

    build: copy `src/blessing-texts` to dist after build

---
+++ ts-project-builder.config.mjs
@@ -0,0 +1,19 @@
+import { cpSync } from 'node:fs';
+
+import { defineConfig } from 'ts-project-builder';
+
+let isBlessingTextsCopied = false;
+export default defineConfig({
+    additionalInputPlugins: {
+        afterBuiltIns: [
+            {
+                name: 'copy-blessing-texts',
+                writeBundle() {
+                    if (isBlessingTextsCopied) return;
+                    isBlessingTextsCopied = true;
+                    cpSync('./src/blessing-texts', './dist/blessing-texts', { recursive: true });
+                },
+            },
+        ],
+    },
+});
