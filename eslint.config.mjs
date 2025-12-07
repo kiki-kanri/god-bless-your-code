a874feb1859b5f372521a30d7d19e9a4adf1747a
kiki-kanri <a470666@gmail.com>
2025-12-08 00:56:13 +0800

    chore: enable vue eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import { antfu } from '@antfu/eslint-config';
 import { createBaseConfigs } from '@kikiutils/eslint-config/base';
+import { createVueConfig } from '@kikiutils/eslint-config/vue';
 
 export default antfu(
     {
         type: 'lib',
         typescript: true,
+        vue: true,
     },
     createBaseConfigs(),
+    createVueConfig(),
     { rules: { 'ts/explicit-function-return-type': 'off' } },
 );
